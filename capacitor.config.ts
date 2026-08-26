d3a545db112f767fd75937a456ceae9624755e53
woyue2 <stephendu@foxmail.com>
2026-08-26 23:10:09 +0800

    feat: configure Capacitor Android build

---
+++ capacitor.config.ts
@@ -0,0 +1,18 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.park.flow01',
+  appName: '思维流',
+  webDir: 'dist',
+  server: {
+    androidScheme: 'https',
+    cleartext: true,
+  },
+  plugins: {
+    CapacitorHttp: {
+      enabled: true,
+    },
+  },
+};
+
+export default config;
